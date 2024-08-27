5fc52801c8767dcf69a503ef6199bd0fef94a4f6
sacuna <sacuna@comscore.com>
2024-08-27 17:26:48 -0400

    Initialize Spring Boot Application

---
+++ src/main/java/com/proyectouwu/pkmnapi/pkmn_api/PokemonApiConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.proyectouwu.pkmnapi.pkmn_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonApiConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonApiConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectouwu/pkmnapi/pkmn_api/PokemonApiConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectouwu.pkmnapi.pkmn_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonApiConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
